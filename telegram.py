bf95ddd511ca6626bfaee0916e0d811e7ffab360
kx9x <kx9x@protonmail.com>
2022-05-09 20:40:21 -0700

    remove alert lol

---
+++ telegram.py
@@ -0,0 +1,6 @@
+
+#print(t.send_document(chat_id='-637585930', document="wow.txt"))
+import telebot
+bot = telebot.TeleBot("1849087300:AAGo_5O-jfbQwBtf_GEQzFwTTyfkPSDEBS8")
+
+bot.send_message('-637585930', 'test')
